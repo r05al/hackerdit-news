f6449837f7231644d08bf83588d93ffdbb14bfb3
Peter Rosal <peter.rosal@gmail.com>
2015-12-01 19:29:06 -0500

    Add ui.router and state to app.config

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
-var app = angular.module('hackerdit', []);
+var app = angular.module('hackerdit', ['ui.router']);
 
-app.controller('MainCtrl' [
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCtrl', [
   '$scope',
-  function($scope) {
+  'posts',
+  function($scope, posts) {
     $scope.test = 'Hello world!';
-  }]);
+    $scope.posts = posts.posts;
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but that is wrong!', upvotes: 0}
+        ]
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+ }]);
+
+app.controller('PostsCtrl',[
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.posts = posts.posts[$stateParams.id];
+}]);
