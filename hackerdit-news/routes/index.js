98a42d30b2b253951534db5943a9ae43236a62f7
Peter Rosal <peter.rosal@gmail.com>
2015-12-03 14:34:50 -0500

    Add auth logic to routes

--- hackerdit-news/routes/index.js
+++ hackerdit-news/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'}); //HARDCODED token
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +12,41 @@ router.get('/', function(req, res, next) {
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+router.post('/register', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please provide username and password'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function(err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+})
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please provide username and password'});
+  }
+
+  passport.authenticate('local', function(err, user, info){ //middleware uses LocalStrategy
+    if(err){ return next(err); }
+
+    if(user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
@@ -18,9 +56,9 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
-
+  post.author = req.payload.username;
   post.save(function(err, post){
     if(err){ return next(err); }
 
@@ -60,7 +98,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -68,10 +106,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
-
+  comment.author = req.payload.username;
   comment.save(function(err, comment){
     if(err){ return next(err); }
 
@@ -84,7 +122,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
